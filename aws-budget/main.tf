ee17b32a6fc2900b2aeddb79054c9fe422f50265
Michael Quinton <32737081+MikeQuinton@users.noreply.github.com>
2024-11-28 22:55:17 +0100

    Removed credentials

---
+++ aws-budget/main.tf
@@ -0,0 +1,31 @@
+# Specifying the AWS provider
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_budgets_budget" "aws_budget" {
+  name              = "monthly-budget"
+  budget_type       = "COST"
+  limit_amount      = "10.0"
+  limit_unit        = "USD"
+  time_unit         = "MONTHLY"
+  time_period_start = "2024-11-28_00:01"
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 80
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "FORECASTED"
+    subscriber_email_addresses = ["subscribe@123456.com"]
+  }
+}
